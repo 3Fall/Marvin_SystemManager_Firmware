164d098f8971806e11e9fc7f6f79783299c956f2
Szum123321 <szymek.januszek@gmail.com>
2022-12-06 12:51:44 +0100

    asm...

--- src/spiSlave.cpp
+++ src/spiSlave.cpp
@@ -34,18 +34,42 @@ void _spi_swap_buffers_unchecked() {
 
 void spi_on_interrupt() {
 	asm volatile (
-		"lds r16, %[spi0data] \n\t"
-		"eor %[state_active], %[state_active]\n\t" //if spi_state.active
-		"sbrc r16, 0\n\t" 
-		"rjmp [inactive]\n\t"
-		"st %a[dma_ptr], r16\n\t"
+		"and %[state_active], %[state_active]\n\t" 
+		"breq inactive\n\t" //if !spi_state.active: goto inactive
+
+		"andi %[spi0intflags], %[spiTXCIEbm]\n\t"
+		"brne transfer_complete\n\t" //if SPI0.INTFLAGS & SPI_TXCIE_bm == 0: goto transfer_complete
+
+		"sbrc %[state_dir], 0\n\t" //if (spi_state.direction) *spi_state.dma_pointer = data;
+		"st %a[dma_ptr], %[spi0data]\n\t"
+
+		"ld %[spi0data], %a[dma_ptr]\n\t" //SPI0.DATA = *spi_state.dma_pointer;
+
+		"inc %A[dma_ptr]\n\t" //inc lower byte of spi_state.dma_pointer
+		"sts %[dma_ptr_ptr], %A[dma_ptr]\n\t"
+
+		"rjmp finish\n\t"
+
 		"inactive:\n\t"
-		"ret\n\t"
+		"bst %[spi0data], 7\n\t" //spi_state.direction = (data >> 7);
+		"bld %[state_dir], 0\n\t"
+		
+		"eor %A[dma_ptr], %A[dma_ptr]\n\t" //spi_state.dma_pointer = spi_state.dma_base_ptr => low(spi_state.dma_pointer) = 0
+		"ld __tmp_reg__, %a[dma_ptr]\n\t" 
+		"mov %A[dma_ptr], %[spi0data]\n\t" //ow(spi_state.dma_pointer) = SPI0.DATA
+		"mov %[spi0data],  __tmp_reg__\n\t" //SPI0.DATA = *spi_state.dma_base_ptr
+
+		"rjmp finish\n\t"
 		"transfer_complete:\n\t"
-		"ret"
-		: 
-		: [spi0data] "i" (SPI0.DATA), [dma_ptr] "e" (spi_state.dma_pointer), [spi0intflags] "i" (&SPI0.INTFLAGS),
-			[state_active] "r" (spi_state.active)
+		"ori %[spi0intflags], %[spiTXCIEbm]\n\t"
+		"ldi %[state_active], 0\n\t"
+		
+
+		"finish:\n\t"
+		: [state_active] "=r" (spi_state.active), [state_dir] "=r" (spi_state.direction), [spi0data] "=r" (SPI0.DATA)
+		: [spiTXCIEbm] "M" (SPI_TXCIE_bm), [dma_ptr_ptr] "i" (&spi_state.dma_pointer), [dma_base_high] "i" ((int)&spi_state.dma_base_address + 1),
+			[dma_ptr] "e" (spi_state.dma_pointer),
+			"[state_active]"  (spi_state.active), "[state_dir]" (spi_state.direction), "[spi0data]" (SPI0.DATA), [spi0intflags] "r" (&SPI0.INTFLAGS)
 		: "r16", "r17"
 	);
 
