e072d6117aecda0ef968fc4005583e628b0a0edd
Szum123321 <szymek.januszek@gmail.com>
2022-12-07 21:52:02 +0100

    added missing symbols

--- include/spiSlave.hpp
+++ include/spiSlave.hpp
@@ -1,4 +1,17 @@
 #pragma once
+ 
+#define _ptr_len 2 //sizeof((void*))
+
+#define SPI_STATE_BASE 0x3400
+#define SPI_STATE_ACTIVE (0x3400 + 2*256)
+#define SPI_STATE_DIRECTION (SPI_STATE_ACTIVE + 1)
+#define SPI_STATE_SHOULD_SWAP (SPI_STATE_DIRECTION + 1)
+#define SPI_STATE_WRITTEN (SPI_STATE_SHOULD_SWAP + 1)
+#define SPI_STATE_DMA_BASE_ADDRESS SPI_STATE_WRITTEN + 1
+#define SPI_STATE_DMA_PTR SPI_STATE_DMA_BASE_ADDRESS + 2
+
+
+#if !defined (__ASSEMBLER__)
 #include <Arduino.h>
 
 struct SPI_STATE_t {
@@ -6,16 +19,16 @@ struct SPI_STATE_t {
 	bool active, direction, shouldSwap, written;
 	uint8_t *dma_base_address, *dma_pointer;
 	uint8_t *access_base_address;
-};
-
-//__attribute__ ((address (0x3500)))
+} __attribute__ ((packed));
 
 //TODO: align to 256-byte boundary
-extern "C" SPI_STATE_t  spi_state;
+extern SPI_STATE_t spi_state __attribute__ ((address (SPI_STATE_BASE)));
 extern void on_spi_write();
 
 void spi_slave_init();
 
 inline bool spi_is_transmitting();
-void spi_on_interrupt();
+extern void spi_on_interrupt();
 void spi_swap_buffers();
+
+#endif
